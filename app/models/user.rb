aea4c523a205269d657f347cb29e71cfb43d0789
jonathan kam <jonathan.kam@protonmail.com>
2017-11-30 16:30:59 +0100

    tables ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :meals
+  has_many :orders
+  has_many :reviews, through: :meals
 end
