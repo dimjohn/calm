14b0e46cdd822d77b8bd3df0358eb1c6a4c8566c
jonathan kam <jonathan.kam@protonmail.com>
2017-12-01 12:42:02 +0100

    facebook ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :meals
-  devise_for :users
   root to: 'meals#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :meals
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201103658) do
+ActiveRecord::Schema.define(version: 20171201112106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 20171201103658) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
